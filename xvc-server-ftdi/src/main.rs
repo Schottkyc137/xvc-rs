d2854054678a927464817efb72aa1b7d1be65966
Lukas Scheller <lukas2121@icloud.com>
2026-07-02 22:32:32 +0200

    Add interactive device-disambiguation

--- xvc-server-ftdi/src/main.rs
+++ xvc-server-ftdi/src/main.rs
@@ -1,14 +1,16 @@
 use clap::Parser;
 use env_logger::Env;
+use inquire::{InquireError, Select};
 use std::{
     error::Error,
+    fmt::Display,
     net::{IpAddr, SocketAddr},
 };
 use tokio::net::TcpListener;
 use tokio_util::sync::CancellationToken;
 use xvc_server::server::{Config, Server};
 
-use crate::ftdi_server::FtdiServer;
+use crate::{ftdi_device::FtdiJtagDevice, ftdi_server::FtdiServer};
 
 mod ftdi_device;
 mod ftdi_server;
@@ -22,11 +24,63 @@ struct Args {
     #[arg(short, long, default_value = "0.0.0.0")]
     ip: IpAddr,
 
+    /// The FTDI port to use
     #[arg(short, long, default_value = "0")]
     ftdi_port: usize,
 
-    #[arg(short, long)]
+    /// Debug flag to operate the FTDI chip in loopback mode
+    #[arg(short, long, default_value_t = false)]
     loopback: bool,
+
+    /// Whether to present the option interactively
+    #[arg(short, long, default_value_t = true)]
+    interactive: bool,
+}
+
+fn disambiguate_available_devices(
+    mut available: Vec<FtdiJtagDevice>,
+    interactive: bool,
+) -> Option<FtdiJtagDevice> {
+    if available.is_empty() {
+        return None;
+    }
+    if available.len() == 1 {
+        return Some(available.pop().unwrap());
+    }
+    if !interactive {
+        log::error!("Multiple devices found");
+        return None;
+    }
+
+    struct Wrapper {
+        device: FtdiJtagDevice,
+    }
+
+    impl Display for Wrapper {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "{}", self.device.info())
+        }
+    }
+
+    let res = Select::new(
+        "Multiple matching devices found",
+        available
+            .into_iter()
+            .map(|avail| Wrapper { device: avail })
+            .collect(),
+    )
+    .prompt()
+    .inspect_err(|err| {
+        if !matches!(
+            err,
+            InquireError::OperationCanceled | InquireError::OperationInterrupted
+        ) {
+            log::error!("{err}");
+        }
+    })
+    .ok()?;
+
+    Some(res.device)
 }
 
 #[tokio::main]
@@ -39,17 +93,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let config = Config::default();
     log::debug!("Server config: max_vector_size={}", config.max_vector_size);
 
-    let mut available_devices =
+    let available_devices =
         ftdi_device::list_available_devices(args.ftdi_port, config.read_write_timeout)?;
 
-    let device = if available_devices.is_empty() {
-        log::error!("No FTDI device detected");
+    let Some(device) = disambiguate_available_devices(available_devices, args.interactive) else {
         return Ok(());
-    } else if available_devices.len() > 1 {
-        // TODO: Choose between different devices interactively and / or allow CLI interaction
-        unimplemented!("more than one device")
-    } else {
-        available_devices.pop().unwrap()
     };
 
     device.ftdi_init(args.loopback)?;
